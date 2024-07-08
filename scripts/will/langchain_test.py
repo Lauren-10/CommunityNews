73564b669eed524c1c410d1d68c402917fbd992b
WillHWThompson <will.w.thompson@gmail.com>
2024-07-08 15:18:36 -0400

    added new tags to langchain scraper

--- scripts/will/langchain_test.py
+++ scripts/will/langchain_test.py
@@ -1,13 +1,13 @@
-#from langchain_community.document_loaders import AsyncHtmlLoader
-#from langchain_community.document_transformers import BeautifulSoupTransformer
-#from langchain_text_splitters import RecursiveCharacterTextSplitter
-#from langchain_community.document_loaders import AsyncChromiumLoader
-#from langchain.chains import create_extraction_chain
-#import pprint
+from langchain_community.document_loaders import AsyncHtmlLoader
+from langchain_community.document_transformers import BeautifulSoupTransformer
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import AsyncChromiumLoader
+from langchain.chains import create_extraction_chain
+import pprint
 from langchain_openai import ChatOpenAI
-from scn_src.lang_chain_utils import extract, scrape_with_playwright
+from scn_src.lang_chain_utils import load_docs,extract_metadata,extract_all_metadata
 
-llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
 
 # Transform
 
@@ -16,22 +16,18 @@ schema = {
         "news_article_title": {"type": "string"},
         "news_article_author": {"type": "string"},
         "news_article_publication_date": {"type": "string"},
+        "is_author_student_college_student": {"type":"boolean"},
+        "is_author_student_journalist": {"type":"boolean"},
+        "is_article_university_collaboration": {"type":"boolean"}
     },
-    "required": ["news_article_title","news_article_summary", "news_article_author"],
+    "required": ["news_article_title","news_article_summary", "news_article_author",'is_author_student_journalist','is_article_university_collaboration','is_author_college_student'],
 }
 
 
-urls = ["https://www.theplainsman.com/article/2024/06/we-will-not-go-at-auburns-pace-university-employees-unionize-for-living-wage", 
-        "https://deltanewsservice.com/2024/05/31/legends-bbq-a-thriving-figure-of-the-nea-black-community/", 
-        "https://cronkite.asu.edu/experiences/cronkite-news-phoenix/", 
-        "https://eldonnews.org/top-stories/2024/05/29/slushcult-is-a-community-built-on-nostalgia/", 
-        "https://oaklandnorth.net/2024/06/24/oakland-mayor-thao-fbi-raid-this-wouldnt-have-gone-down-the-way-it-did-if-i-was-rich/", 
-        "https://richmondconfidential.org/2024/07/06/richmond-art-center-view-from-here-art-exhibit-offers-prisoners-perspective/", 
-        "https://xtown.la/2024/06/25/more-street-racing-and-sideshows-in-los-angeles-but-few-effective-laws/", 
-        "https://www.uscannenbergmedia.com/2024/07/06/barbie-laments-everything-respects-nothing/",  
-        "https://hunewsservice.com/local/dispensaries-open-using-legal-loopholes/",
-        "https://www.theplainsman.com/article/2024/05/auburn-mens-golf-team-secures-first-national-championship-caps-historic-season",
-        "https://www.wvua23.com/take-a-look-at-tuscaloosas-newest-attraction-popstroke-minigolf/",
-        "https://zunews.com/2024/04/issues-with-commuting-and-off-campus-housing/"]
+urls = ['https://vtdigger.org/2024/02/11/anglers-flock-to-lake-elmore-for-free-ice-fishing-day/','https://vtdigger.org/2023/11/12/construction-of-walk-bike-bridge-over-interstate-now-on-for-2024/',
+        "https://www.theplainsman.com/article/2024/05/auburn-mens-golf-team-secures-first-national-championship-caps-historic-season","https://cronkitenews.azpbs.org/2024/05/30/coaches-arizona-poor-numbers-youth-sports-participation/"]
 
-extracted_content = scrape_with_playwright(urls, schema=schema,llm = llm)
+
+tags_to_extract = ['p','span','a','div']
+
+extracted_tags = extract_all_metadata(urls,llm,schema)
