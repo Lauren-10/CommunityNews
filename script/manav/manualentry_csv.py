704d1cadeb90b958f8679b2de1929f275330136a
manav-adh <manav.adh@gmail.com>
2024-07-17 10:08:16 -0400

    changes made

--- script/manav/manualentry_csv.py
+++ script/manav/manualentry_csv.py
@@ -4,3 +4,5 @@ df= pd.read_csv(file_location)
 if [df['Is_student_journalist '].notnull()]:
     df_ground_truth = df[['Article Name', 'URL ','Partnership ','Is_student_journalist ']]
 print(df_ground_truth.head())
+df_ground_truth = df_ground_truth.rename(columns={'Article Name' : 'news_article_title', 'URL ' : 'urls', 'Partnership' : 'partnership', 'Is_student_journalist ' : 'is_student_reported'})                            
+df_ground_truth.to_csv('ground_truth_df.csv')
