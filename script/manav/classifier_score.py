aa4292af6b35748027d1bff66308d06418a385c0
manav-adh <manav.adh@gmail.com>
2024-07-24 11:59:02 -0400

    changes added

--- script/manav/classifier_score.py
+++ script/manav/classifier_score.py
@@ -1,24 +1,43 @@
 import pandas as pd 
+import numpy as np
+df_scraper = pd.read_csv('scraper_dataframe.csv')
+df_ground_truth = pd.read_csv('ground_truth_df.csv')
+
+
 #function that merges the dataframe and returns score function with bolean values 
 def calculate_classifier_score(df_scraper, df_ground_truth, score_function):
-    merged_dataframe = pd.merge(df_ground_truth, df_scraper[['Is_student_reported']], on='urls', how='left')
-    scraper_classification = merged_dataframe['is_student_reported_scraper']
+    merged_dataframe = pd.merge(df_scraper, df_ground_truth[['is_student_reported', 'urls']], on='urls', how='left', suffixes=('_scraper', '_ground_truth'))
     ground_truth_classification = merged_dataframe['is_student_reported_ground_truth']
+    scraper_classification = merged_dataframe['is_student_reported_scraper']
     return score_function(scraper_classification, ground_truth_classification)
 
-#function for percision what parameters does it take? 
+#function for percision and recall
 def precision_recall(scraper_classification, ground_truth_classification):
-    TP = ((scraper_classification == True) & (ground_truth_classification == True)).sum()
-    FP = ((scraper_classification == True)) & (ground_truth_classification == False).sum()
-    FN = ((scraper_classification == True)) & (ground_truth_classification == False).sum()
-    TN = ((scraper_classification == False)) & (ground_truth_classification == False).sum()
-    precision = (TP / (FN + TP)) if (FN + FP) > 0 else 0
-    recall = (TP / (FN + TP)) if (FN + TP ) > 0 else 0 
+    """takes in two dataframes, zips it and each boolean from the zip corresponds to g and s respectively 
+    g and s is true if both g,s = true, true
+    not g and s is true if g,s = false, true 
+    g and not s is true if g,s = true, false"""
+    TP = sum((g and s) for g,s in zip(ground_truth_classification, scraper_classification))
+    FP = sum((not g and s) for g, s in zip(ground_truth_classification, scraper_classification))
+    FN = sum((g and not s) for g,s in zip(ground_truth_classification, scraper_classification))
+    precision = ((TP) / (FP + TP)) if (FN + FP) > 0 else 0
+    recall = ((TP) / (FN + TP)) if (FN + TP ) > 0 else 0 
+    standard_deviation = scraper_classification.std()
+    print("standard deviation is", standard_deviation)
     return precision, recall  
 
-#function for f1_score 
+"""function for f1_score,
+calls percision and recall function from above, and assigns values to variables to work with"""
+
 def f1_score(scraper_classification, ground_truth_classification):
-     precision, recall = percision_recall(scraper_classification, ground_truth_classification)
-     score = (precision * recall) / (precision + recall)
+     precision, recall = precision_recall(scraper_classification, ground_truth_classification)
+     score = 2 * ((precision * recall) / (precision + recall)) if (precision + recall) > 0 else 0
      return score 
 
+score_value = calculate_classifier_score(df_scraper, df_ground_truth, precision_recall)
+
+#print
+if score_value == calculate_classifier_score(df_scraper, df_ground_truth, f1_score):
+    print(f"the f1 score value is {score_value}")
+else:
+    print(f"precision and recall is {score_value}")
