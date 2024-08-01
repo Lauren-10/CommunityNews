5569f8e2b3c68f83a3e9a4904d9914fba121a141
Lauren Knopp <lauren.knopp0@gmail.com>
2024-08-01 09:33:55 -0400

    Final prompt as a function

--- scn_src/prompt_draft.py
+++ scn_src/prompt_draft.py
@@ -4,46 +4,65 @@ from langchain_core.prompts import (
 )
 from langchain_openai import ChatOpenAI
 
+def prompt():
+    examples = [
+        {"input": """"This story was produced by Fresh Take Florida,
+    a news service of the University of Florida College of Journalism and Communications.
+    The reporter can be reached at vivienneserret@ufl.edu.
+    You can donate to support our students here
+    """, "output": "Vivienne Serret, True"},
+        {"input": "By WVUA 23 Student News Reporter Nick Balenger", "output": "Nick Balenger, True"},
+        {"input": "Saman Shafiq is a trending news reporter for USA TODAY. Reach her at sshafiq@gannett.com and follow her on X @saman_shafiq7.", "output": "Saman Shafiq,False"},
+        {"input": """Noah Biesiada is a Voice of OC reporter and corps member with Report for America,
+    a GroundTruth initiative. 
+    Contact him at nbiesiada@voiceofoc.org or on Twitter @NBiesiada.
+    """, "output": "Noah Biesada,False"},
+        {"input": "Melanie Mendez is a former student reporter at KUNR Public Radio.", "output": "Melanie Mendez, False"},
+        {"input": """BY ANGELINA HICKS AND GRETA CIFARELLI...
+        This dispatch is part of the Voice of OC Collegiate News Service, 
+        working with student journalists to cover public policy issues across Orange County. 
+        If you would like to submit your own student media project related to Orange County civics or if you have any response to this work,
+        contact admin@voiceofoc.org.""", "output": "Angelina Hicks and Greta Cifarelli,True"},
 
-examples = [
-    {"input": """"This story was produced by Fresh Take Florida,
-a news service of the University of Florida College of Journalism and Communications.
-The reporter can be reached at vivienneserret@ufl.edu.
-You can donate to support our students here
-""", "output": "True"},
-    {"input": "By WVUA 23 Student News Reporter Nick Balenger", "output": "True"},
-    {"input": "Saman Shafiq is a trending news reporter for USA TODAY. Reach her at sshafiq@gannett.com and follow her on X @saman_shafiq7.", "output": "False"},
-    {"input": """Noah Biesiada is a Voice of OC reporter and corps member with Report for America,
-a GroundTruth initiative. 
-Contact him at nbiesiada@voiceofoc.org or on Twitter @NBiesiada.
-""", "output": "False"},
-    {"input": "Melanie Mendez is a former student reporter at KUNR Public Radio.", "output": "False"},
 
-
-]
-# This is a prompt template used to format each individual example.
-example_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("human", "{input}"),
-        ("ai", "{output}"),
     ]
-)
-few_shot_prompt = FewShotChatMessagePromptTemplate(
-    example_prompt=example_prompt,
-    examples=examples,
-)
-#want to return final prompt
-#print(few_shot_prompt.format())
-final_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", """You are an AI assistant that can classify articles
-         as student written or reporter written, respond false if the author
-         is a professional journalist or former student, true if the author is a university student"""),
-        few_shot_prompt,
-        ("human", "{input}"),
-    ]
-)
-chain = final_prompt | ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+    # This is a prompt template used to format each individual example.
+    example_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("human", "{input}"),
+            ("ai", "{output}"),
+        ]
+    )
+    few_shot_prompt = FewShotChatMessagePromptTemplate(
+        example_prompt=example_prompt,
+        examples=examples,
+    )
+    #want to return final prompt
+    #print(few_shot_prompt.format())
+    final_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", """You are an AI assistant that can read and digest articles
+            and pick up on two variables and place them into a comma seperated list:
+            firstly, the author's name, identify the First and Last name
+            of the article author.
+            second, whether or not the article is student written
+            respond false if the author
+            is a professional journalist, former student, or ambiguous, 
+            true if the author is a university student.
+            Second, the author's name, identify the First and/or Last name
+            of the article author.
+            In the event that two or more authors appear, format the output as so:
+            "Name and Name and Name, boolean". if one author on the article is a student, consider
+            that article to be student written
+            """),
+            few_shot_prompt,
+            ("human", "{input}"),
+        ]
+    )
+    return final_prompt
 
-print(chain.invoke({"input": """This story is jointly published by nonprofits Amplify Utah and The Salt Lake Tribune,
-                     in collaboration with the University of Utah, to elevate diverse perspectives in local media through student journalism."""}))
+chain = prompt() | ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+
+print(chain.invoke({"input": """Lucia McCallum interns as the Hardwick Gazette’s community resilience reporter
+                     with support from the Leahy Institute for Rural Partnerships. She works with editors at Community News Service,
+                     a University of Vermont journalism program."""}).content)
